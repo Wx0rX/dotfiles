e95dff1135f3331dcec37f56350720fced68bfbe
funkyMonks1338 <71926823+funkyMonks1338@users.noreply.github.com>
2023-01-13 00:24:03 +0500

    added ~/cppOlympTemplate.cpp
    insert this into ~/cppOlympTemplate.cpp. Use "cp ~/cppOlympTemplate.cpp A.cpp" when you solve problems

---
+++ cppOlympTemplate.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fast                                                                   \
+    ios_base::sync_with_stdio(false);                                          \
+    cin.tie(0);                                                                \
+    cout.tie(0)
+#define sz(a) (int)a.size()
+#define all(a) a.begin(), a.end()
+#define vc vector
+#define F first
+#define S second
+#define forin(i, a, b) for (int i = a; i < b; ++i)
+#define readgraph(n, g)                                                        \
+    for (int i = 0; i < n; ++i) {                                              \
+        int u, v;                                                              \
+        cin >> u >> v;                                                         \
+        --u;                                                                   \
+        --v;                                                                   \
+        g[u].push_back(v);                                                     \
+        g[v].push_back(u);                                                     \
+    }
+#define int long long
+
+constexpr int INF = numeric_limits<int>::max() >> 1;
+
+template <class T> istream &operator>>(istream &is, vector<T> &arr) {
+    for (auto &a : arr)
+        is >> a;
+    return is;
+}
+
+signed main() {
+    return 0;
+}
