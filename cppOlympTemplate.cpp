0e1f3ba5523fc4ae7a75b87fabbdd791990dce99
funkyMonks1338 <71926823+funkyMonks1338@users.noreply.github.com>
2023-01-13 20:46:12 +0500

    added ordered_set to ~/cppOlympTemplate.cpp
    https://codeforces.com/blog/entry/11080?locale=ru
    This data structure is the same as "std::set", but there are new functions:
    1) ordst.find_by_order(i) - get the iterator by index i
    2) ordst.order_of_key(x) - get the index, by the value x

--- cppOlympTemplate.cpp
+++ cppOlympTemplate.cpp
@@ -1,7 +1,12 @@
 #include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
 
 using namespace std;
+// using namespace __gnu_pbds;
 
+// #define ordered_set(T)                                                         \
+//     tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>
 #define fast                                                                   \
     ios_base::sync_with_stdio(false);                                          \
     cin.tie(0);                                                                \
