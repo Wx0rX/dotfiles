ff6ae90b1485432471dc2d0b35629983102832d4
Wx0rX <71926823+Wx0rX@users.noreply.github.com>
2024-08-29 21:16:29 +0500

    blablabla

--- explo_pwn.py
+++ explo_pwn.py
@@ -2,13 +2,22 @@
 # -*- coding: utf-8 -*-
 import pathlib
 import pwn as w
-w.context.update(encoding="utf-8", arch="amd64", terminal=["tmux", "splitw", "-h"])  # , log_level="debug")
-elf = w.ELF("./patched_chall")  # if w.args.LOCAL else ...
-# libc = w.ELF("./libc.so.6")
-with (elf.process([], env={}) if w.args.LOCAL else w.remote("127.0.0.1", 31337)) as io:
-    sla = io.sendlineafter; sl = io.sendline; ru = io.recvuntil; rl = io.recvline; inter = io.interactive
-    if w.args.GDB: w.gdb.attach(io, gdbscript="""
+w.context.update(encoding="utf-8",bits=64,arch="amd64",terminal=["tmux","splitw","-h"])
+w.tube.sla=w.tube.sendlineafter;w.tube.sa=w.tube.sendafter;w.tube.sl=w.tube.sendline;w.tube.ru=w.tube.recvuntil;w.tube.rl=w.tube.recvline;w.tube.inter=w.tube.interactive
+EP = "./patched_chall"
+LP = "./libc.so.6"
+elf = w.ELF(EP)  # if w.args.LOCAL else ...
+libc = w.ELF(LP)  # if w.args.LOCAL else ...
+# wrapper:
+def ATTACH_GDB():
+    if w.args.GDB: w.gdb.attach(io,gdbscript="""
+    # # noaslr
+    # b *main
+    # b *(0x0000555555554000+0x1337)
     c
     """)
-    # print(f"[+]: {leak = :#x}")
-    inter()
+# exploit:
+with (w.process(["./ld-linux-x86-64.so.2",EP],env={"LD_PRELOAD":LP}) if w.args.LOCAL else w.remote("127.0.0.1",5000)) as io:
+    ATTACH_GDB()
+
+    io.inter()
